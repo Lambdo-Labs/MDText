15d64ef1420fd31c1b39c1bc86f6c003e5b92f01
Andre Carrera <acarrera94@gmail.com>
2019-10-09 11:38:01 -0600

    Initial Commit

---
+++ MDText/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MDText
+//
+//  Created by Andre Carrera on 10/9/19.
+//  Copyright © 2019 Lambdo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
